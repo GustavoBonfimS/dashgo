156798e535e29ef9c6e6abcfe3a4a967ecdca6aa
Gustavo Bonfim <gustavo.hbonfim@outlook.com>
2022-07-25 20:43:23 -0300

    feat: formulário e paginação

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,49 @@
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
+
+import { Header } from "../../components/Header";
+import { SideBar } from "../../components/Sidebar";
+
+function CreateUser() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <SideBar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Heading size="lg" fontWeight="normal">
+            Criar usuário
+          </Heading>
+
+          <Divider my="6" borderColor="gray.700" />
+
+          <VStack spacing={8}>
+            <SimpleGrid minChildWidth="240px" spacing={8} w="full">
+              <Input name="name" label="Nome completo" />
+              <Input name="email" label="E-mail" type="email" />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth="240px" spacing={8} w="full">
+              <Input name="password" type="password" label="Senha" />
+              <Input name="password_confirmation" type="password" label="Confirmação da senha" />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt={8} justify="flex-end">
+            <HStack spacing={4}>
+              <Button colorScheme="whiteAlpha">
+                Cancelar
+              </Button>
+              <Button colorScheme="pink">
+                Salvar
+              </Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
+
+export default CreateUser;
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
+import Pagination from "../../components/Pagination";
 import { SideBar } from "../../components/Sidebar";
 
 function UserList() {
@@ -133,6 +134,8 @@ function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
